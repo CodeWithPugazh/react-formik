aa45642541ca4715eaf181d3c562f5abef7a79fc
Pugazhenthi <pugazhonline@gmail.com>
2020-07-09 20:54:03 +0530

    Formik hook example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values, { setSubmitting, resetForm, setErrors }) => {
+      setSubmitting(true);
+
+      //to simulate network call
+      setTimeout(() => {
+        alert(JSON.stringify(values));
+        // setErrors({ email: "Error may set after sumbitting" });
+        setSubmitting(false);
+        resetForm();
+      }, 300);
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .min(3, "Name should be minimum of 3 characters.")
+        .required("Required."),
+      email: Yup.string()
+        .email("Please enter valid email.")
+        .required("Required"),
+      password: Yup.string()
+        .required("Required")
+        .min(6, "Password should be minimum of 6 charecters long"),
+    }),
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <h3>React Forms validation using Formik and yup</h3>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+          {formik.errors.name && formik.touched.name ? (
+            <p className="error">{formik.errors.name}</p>
+          ) : null}
+        </div>
+
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          {formik.errors.email && formik.touched.email ? (
+            <p className="error">{formik.errors.email}</p>
+          ) : null}
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            onChange={formik.handleChange}
+            value={formik.values.channel}
+          />
+          {formik.errors.password && formik.touched.password ? (
+            <p className="error">{formik.errors.password}</p>
+          ) : null}
+        </div>
+        <button type="submit" disabled={formik.isSubmitting}>
+          {formik.isSubmitting ? "Loading..." : "Submit"}
+        </button>
+      </form>
     </div>
   );
 }
